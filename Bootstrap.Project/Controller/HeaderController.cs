69806024cd98902ad1073b5f0785fc6799f13162
ukesh10 <ukeshsharma10@gmail.com>
2023-05-24 21:41:32 +0530

    First Commit

---
+++ Bootstrap.Project/Controller/HeaderController.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.IO;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows.Forms;
+
+namespace Bootstrap.Project.Controllers
+{
+    public class HeaderController : Controller
+    {
+        // GET: Hero
+        public ActionResult Index()
+        {
+            var dataItem = RenderingContext.Current.Rendering.Item;
+            return View("~/Views/Bootstrap/Components/Header.cshtml",dataItem);
+        }
+    }
+}
---
+++ Bootstrap.Project/Controller/TeamController.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.IO;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows.Forms;
+
+namespace Bootstrap.Project.Controllers
+{
+    public class TeamController : Controller
+    {
+        // GET: Hero
+        public ActionResult Index()
+        {
+            var dataItem = RenderingContext.Current.Rendering.Item;
+            var slideItem = (MultilistField)dataItem.Fields["slides"];
+            var slides = slideItem.GetItems();
+            return View("~/Views/Bootstrap/Components/Team.cshtml",slides);
+        }
+    }
+}
